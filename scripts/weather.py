e6b43113713563eca8e3876d7db234b92f6c41ac
Ralf Treinen <treinen@pps.univ-paris-diderot.fr>
2014-05-27 13:18:43 +0200

    generate weather report per arch in xml format

--- scripts/weather.py
+++ scripts/weather.py
@@ -5,8 +5,44 @@
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 
-import os
+import os, datetime
 from common import *
+
+xml_template='''<weather>
+  <bundle>
+    <id>{scenario}</id>
+    <description>{description}</description>
+  </bundle>
+  <architecture>{architecture}</architecture>
+  <date>{date}</date>
+  <packages>
+    <total>{number_total}</total>
+    <broken>{number_broken}</broken>
+  </packages>
+  <index>{weather}</index>
+  <url>{summary_url}</url>
+</weather>'''
+
+def weather_index(percentage):
+    '''
+    return a weather index for a percentage of broken packages
+    1: sunny
+    2: light clouds
+    3: overcast
+    4: showers
+    5: storm
+    '''
+
+    if percentage <= 1:
+        return(1)
+    elif percentage <= 2:
+        return(2)
+    elif percentage <= 3:
+        return(3)
+    elif percentage <= 4:
+        return(4)
+    else:
+        return(5)
 
 def build(timestamp,scenario,architectures):
     for arch in architectures:
@@ -30,6 +66,15 @@ def build(timestamp,scenario,architectures):
             percentage=100*broken_packages/total_packages
 
         with open_weatherfile(scenario,arch,'w') as outfile:
-            print(percentage,file=outfile)
+            print(xml_template.format(
+                    scenario=scenario,
+                    description=scenario,
+                    architecture=arch,
+                    date=datetime.date.fromtimestamp(float(timestamp)),
+                    number_total=total_packages,
+                    number_broken=broken_packages,
+                    weather=weather_index(percentage),
+                    summary_url=url_summary(timestamp,scenario,arch)),
+                  file=outfile)
 
     
