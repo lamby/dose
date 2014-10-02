ee4b554911759d57298d61b3ec236c4dc607596b
Ralf Treinen <treinen@free.fr>
2014-10-02 03:47:08 +0200

    diffs: simplify imports

--- scripts/diffs.py
+++ scripts/diffs.py
@@ -5,7 +5,7 @@
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 
-import yaml,os, hashlib, datetime
+import os
 import html
 from common import *
 
