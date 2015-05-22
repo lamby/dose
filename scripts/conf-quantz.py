4675cab63dc5d972bf25707a6e7e835af6607f12
Ralf Treinen <treinen@free.fr>
2015-05-22 08:00:26 +0200

    dose : disable builddependency checks on sid AND testing

--- scripts/conf-quantz.py
+++ scripts/conf-quantz.py
@@ -70,22 +70,23 @@ scenarios = [
         'bgs'   : [ '{m}/testing/main/binary-{a}/Packages.gz' ],
         'description' : 'Debian testing (contrib and non-free only)'
     },
-    {
-        'name'  : 'src_unstable_main',
-        'type'  : 'source',
-        'archs' : architectures['unstable'],
-        'src'   : '{m}/unstable/main/source/Sources.gz',
-        'bins'  : [ '{m}/unstable/main/binary-{a}/Packages.gz' ],
-        'description' : 'Build-dependencies unstable (main only)'
-    },
-    {
-        'name'  : 'src_testing_main',
-        'type'  : 'source',
-        'archs' : architectures['testing'],
-        'src'   : '{m}/testing/main/source/Sources.gz',
-        'bins'  : [ '{m}/testing/main/binary-{a}/Packages.gz' ],
-        'description' : 'Build-dependencies testing (main only)'
-    },
+    # temporarily disabled
+    # {
+    #     'name'  : 'src_unstable_main',
+    #     'type'  : 'source',
+    #     'archs' : architectures['unstable'],
+    #     'src'   : '{m}/unstable/main/source/Sources.gz',
+    #     'bins'  : [ '{m}/unstable/main/binary-{a}/Packages.gz' ],
+    #     'description' : 'Build-dependencies unstable (main only)'
+    # },
+    # {
+    #     'name'  : 'src_testing_main',
+    #     'type'  : 'source',
+    #     'archs' : architectures['testing'],
+    #     'src'   : '{m}/testing/main/source/Sources.gz',
+    #     'bins'  : [ '{m}/testing/main/binary-{a}/Packages.gz' ],
+    #     'description' : 'Build-dependencies testing (main only)'
+    # },
 ]
 
 # number of runs that are diplays
